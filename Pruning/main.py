e6cb0f8c82ce4bbe08831326aebd92aff5d7c72e
kinan11 <kingamiszczak1@op.pl>
2024-03-26 17:22:11 +0100

    test

--- Pruning/main.py
+++ Pruning/main.py
@@ -9,16 +9,16 @@ from CGA.kernel_density_estimator import gradient, kernel_density_estimator
 
 
 def main():
-    mean1 = [0, -1]
+    mean1 = [-1, 0]
     mean2 = [1, 0]
-    # mean3 = [0, 0, -1]
+    mean3 = [0, -1]
 
-    cluster1 = [np.random.normal(mean1) + [0, -10] for _ in range(20)]
-    cluster2 = [np.random.normal(mean2) + [10, 0] for _ in range(10)]
-    # cluster3 = [np.random.normal(mean3) + [0, 10, -10] for _ in range(10)]
+    cluster1 = [np.random.normal(mean1) + [-10, -10] for _ in range(1)]
+    cluster2 = [np.random.normal(mean2) + [10, 0] for _ in range(1)]
+    cluster3 = [np.random.normal(mean3) + [0, 100] for _ in range(1)]
 
-    # data = np.concatenate((cluster1, cluster2, cluster3), axis=0)
-    data = np.concatenate((cluster1, cluster2), axis=0)
+    data = np.concatenate((cluster1, cluster2, cluster3), axis=0)
+    # data = np.concatenate((cluster1, cluster2), axis=0)
 
     np.random.shuffle(data)
 
@@ -38,24 +38,22 @@ def main():
 
     x = scaler.fit_transform(x)
 
+    colors = plt.cm.rainbow(np.linspace(0, 1, len(x)))
+
+    fig2 = plt.figure()
+    ax2 = fig2.add_subplot(111, projection='3d')
+    ax2.scatter(x[:, 0], x[:, 1], marker='o')
+    plt.show()
+
     z = cluster_algorithm(x)
-    print(z)
 
-    colors = plt.cm.rainbow(np.linspace(0, 1, len(z)))
-
-    # Wizualizacja wyników
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
-    # Rysowanie punktów z różnych klastrów w odpowiednich kolorach
     for cluster_indices, color in zip(z, colors):
         for index in cluster_indices:
             ax.scatter(data[index, 0], data[index, 1], c=[color], marker='o')
 
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_zlabel('Z')
-    ax.set_title('Punkty z różnych klastrów w różnych kolorach')
     plt.show()
 
 
